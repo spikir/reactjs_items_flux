575559a5f7f02fab69c4a5b02a7380c856d2dc2e
spikir <spikirard@gmail.com>
2018-05-24 14:57:47 +0200

    Input field for changing note

--- views/AppView.js
+++ views/AppView.js
@@ -4,10 +4,77 @@ function AppView(props) {
   return (
     <div className="container">
       <Header {...props} />
-      <Main {...props} />
+      <Main
+        mainProp={props.items}
+        update={props.onUpdateItem}
+        delete={props.onDeleteItem}
+      />
       <Footer {...props} />
     </div>
   );
+}
+
+class Main extends React.Component {
+  constructor(props) {
+    super(props);
+    this.props = props;
+    this.state = {
+      input: "",
+      value: false
+    };
+  }
+  handleChange(e) {
+    this.setState({ input: e.target.value });
+  }
+
+  addValue(text, e) {
+    this.setState({ input: text, value: true });
+  }
+  render() {
+    return (
+      <section id="main">
+        <ul className="item-list">
+          {this.props.mainProp.reverse().map(todo => (
+            <li className="item" key={todo.id}>
+              <input
+                className="itemCheck"
+                type="checkbox"
+                checked={todo.complete}
+                onChange={
+                  // Empty function for now, we will implement this later.
+                  () => {
+                    this.props.update(todo.id);
+                  }
+                }
+              />
+
+              {this.state.value === true && (
+                <input
+                  type="text"
+                  value={this.state.input}
+                  onChange={this.handleChange.bind(this)}
+                />
+              )}
+              {this.state.value === false && (
+                <label
+                  className="itemText"
+                  onDoubleClick={this.addValue.bind(this, todo.text)}
+                >
+                  {todo.text}
+                </label>
+              )}
+              <button
+                className="deleteNote"
+                onClick={() => this.props.delete(todo.id)}
+              >
+                Delete Item
+              </button>
+            </li>
+          ))}
+        </ul>
+      </section>
+    );
+  }
 }
 
 function Header(props) {
@@ -15,39 +82,6 @@ function Header(props) {
     <header id="header">
       <h1>todos</h1>
     </header>
-  );
-}
-
-function Main(props) {
-  if (props.items.size === 0) {
-    return null;
-  }
-  return (
-    <section id="main">
-      <ul className="item-list">
-        {[...props.items.values()].reverse().map(todo => (
-          <li className="item" key={todo.id}>
-            <input
-              className="itemCheck"
-              type="checkbox"
-              checked={todo.complete}
-              onChange={
-                // Empty function for now, we will implement this later.
-                () => {
-                  props.onUpdateItem(todo.id);
-                }
-              }
-            />
-            <label className="itemText">{todo.text}</label>
-
-            <button
-              className="deleteNote"
-              onClick={() => props.onDeleteItem(todo.id)}
-            />
-          </li>
-        ))}
-      </ul>
-    </section>
   );
 }
 
